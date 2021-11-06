9dcdc4668f93c1e2cd56ce5fbe231d708b01483a
Daniel Yordanov <daniyord@gmail.com>
2021-11-06 19:46:17 +0200

    -

--- packages/cra/src/App.js
+++ packages/cra/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import GetWelcomeString from "commonasd/src/main";
+import GetWelcomeString from "./common/GetWelcomeString";
 
 function App() {
   return (
